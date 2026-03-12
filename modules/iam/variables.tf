4df28f9846ddb97af9354c4286f08b4b1d55642f
adiallo47 <amadou.d15@yahoo.com>
2026-03-11 21:32:17 -0500

    updated ssh key variable

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   type        = string
   description = "the AWS region where resources will be created"
 }
+
+variable "public_key" {
+  type        = string
+  description = "the public key"
+}
